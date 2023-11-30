3148fbf23004747649e0927b2d0e85c275538493
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2023-11-30 16:29:45 -0500

    created and tested page to delete glass

--- client/src/pages/DeleteGlass.jsx
+++ client/src/pages/DeleteGlass.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function DeleteGlass() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  
+  const handleDeleteGlass = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/glass/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check the console');
+        console.log(error)
+      });
+  }
+
   return (
-    <div>DeleteGlass</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3x1 my-4'>Delete Glass</h1>  
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-x1 w-[600px] p-8 mx-auto'>
+        <h3 className='text-2x1'>Are you sure you want to delete this glass?</h3>
+
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteGlass}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
   )
 }
