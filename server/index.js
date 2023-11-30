3c247095ea80fe1a27d6ee0f891fb934e9dc441d
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2023-11-30 11:32:40 -0500

    moved routes to dedicated folder and allowed CORS

--- server/index.js
+++ server/index.js
@@ -1,120 +1,28 @@
-import express, { response } from "express";
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors'
+
 // move to .env
 import { PORT, mongoDBUrl } from "./config.js";
-import mongoose from "mongoose";
 import { Glass } from './models/glassModel.js'
+import glassRoutes from './routes/glassRoutes.js';
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 
 app.get('/', (req,res) => {
     console.log(req)
     return res.status(234).send('MERN Tutorial')
 });
 
-// create new glass
-app.post('/glass', async (req, res) => {
-try {
-
-    if (
-        !req.body.title ||
-        !req.body.availability ||
-        !req.body.price ||
-        !req.body.quantity
-    ) {
-        return res.status(400).send({
-            message: 'Send all required fields: title, availability, price, and quantity'
-        })
-    }
-
-    // testing glass creation
-    const newGlass = {
-        title: req.body.title,
-        availability: req.body.availability,
-        price: req.body.price,
-        quantity: req.body.quantity,
-    };
-
-    const glass = await Glass.create(newGlass)
-
-    return res.status(201).send(glass)
-    
-} catch (error) {
-    console.log(error.message)
-    response.status(500).send({message: error.message})
-}
-})
-
-// get all glasses from db
-app.get('/glasses', async (req,res) => {
-    try {
-        const glasses = await Glass.find({})
-
-        return res.status(200).json({
-            count: glasses.length,
-            data: glasses
-        })
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message})
-    }
-})
-
-// get one glass from db
-app.get('/glass/:id', async (req,res) => {
-    try {
-        const {id} = req.params;
-        const glass = await Glass.findById(id)
-
-        return res.status(200).json(glass)
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message})
-    }
-})
-
-// update glass
-app.put('/glass/:id', async (req, res) => {
-    try {
-        if (
-            !req.body.title ||
-            !req.body.availability ||
-            !req.body.price ||
-            !req.body.quantity
-        ) {
-            return res.status(400).send({
-                message: 'Send all required fields: title, availability, price, and quantity'
-            })
-        }
-
-        const {id} = req.params.id;
-        const result = await Glass.findByIdAndUpdate(id, req.body);
-
-        if (!result) {
-            return res.status(404).json({message: "Glass not found"})
-        }
-        return res.status(200).send({message: 'Glass successfully updated'})
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({message: error.message})
-    }
-})
-
-// route to delete book
-app.delete('/glass/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const result = await Glass.findByIdAndDelete(id);
-        if(!result) {
-            return res.status(404).json({message: "Glass not found"});
-        }
-        return res.status(200).send({message: "Glass deleted successfully"})
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message})
-    }
-})
+app.use('/glass', glassRoutes)
 
 mongoose.connect(mongoDBUrl)
     .then(() => {
---
+++ server/routes/glassRoutes.js
@@ -0,0 +1,112 @@
+import express from 'express';
+import {Glass} from '../models/glassModel.js'
+
+const router = express.Router();
+
+// create new glass
+router.post('/', async (req, res) => {
+try {
+
+    if (
+        !req.body.title ||
+        !req.body.availability ||
+        !req.body.price ||
+        !req.body.quantity
+    ) {
+        return res.status(400).send({
+            message: 'Send all required fields: title, availability, price, and quantity'
+        })
+    }
+
+    // testing glass creation
+    const newGlass = {
+        title: req.body.title,
+        availability: req.body.availability,
+        price: req.body.price,
+        quantity: req.body.quantity,
+    };
+
+    const glass = await Glass.create(newGlass)
+
+    return res.status(201).send(glass)
+    
+} catch (error) {
+    console.log(error.message)
+    response.status(500).send({message: error.message})
+}
+})
+
+// get all glasses from db
+router.get('/', async (req,res) => {
+    try {
+        const glasses = await Glass.find({})
+
+        return res.status(200).json({
+            count: glasses.length,
+            data: glasses
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+})
+
+// get one glass from db
+router.get('/:id', async (req,res) => {
+    try {
+        const {id} = req.params;
+        const glass = await Glass.findById(id)
+        console.log(id, glass)
+        if(!glass){
+            return res.status(404).json({message: "Glass not found"})
+        }
+        return res.status(200).json(glass)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+})
+
+// update glass
+router.put('/:id', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.availability ||
+            !req.body.price ||
+            !req.body.quantity
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, availability, price, and quantity'
+            })
+        }
+
+        const {id} = req.params.id;
+        const result = await Glass.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).json({message: "Glass not found"})
+        }
+        return res.status(200).send({message: 'Glass successfully updated'})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+})
+
+// route to delete book
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await Glass.findByIdAndDelete(id);
+        if(!result) {
+            return res.status(404).json({message: "Glass not found"});
+        }
+        return res.status(200).send({message: "Glass deleted successfully"})
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+export default router;
