ef5e9531d3b2905a678b339aebc2c5fc4e82287e
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2023-12-05 14:04:19 -0500

    updated deleteGlass to be more felxible and return user without deleting glass

--- client/src/pages/DeleteGlass.jsx
+++ client/src/pages/DeleteGlass.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import BackButton from "../components/BackButton";
-import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSnackbar } from "notistack";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 export default function DeleteGlass() {
   const [loading, setLoading] = useState(false);
@@ -29,21 +29,31 @@ export default function DeleteGlass() {
       });
   };
 
+  const handleNotDeleteGlass = () => {
+    navigate("/");
+  };
+
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3x1 my-4">Delete Glass</h1>
+      <h1 className="text-3x1 my-4 flex place-content-center">Delete Glass</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col items-center border-2 border-sky-400 rounded-x1 w-[600px] p-8 mx-auto">
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl p-8 mx-auto w-5/6 lg:w-2/3">
         <h3 className="text-2x1">
           Are you sure you want to delete this glass?
         </h3>
 
         <button
-          className="p-4 bg-red-600 text-white m-8 w-full"
+          className="p-4 bg-red-600 text-white m-8 w-full rounded-lg hover:bg-red-700"
           onClick={handleDeleteGlass}
         >
-          Yes, Delete it
+          Yes, Delete It
+        </button>
+        <button
+          className="p-4 bg-green-600 text-white m-8 w-full  rounded-lg hover:bg-green-700"
+          onClick={handleNotDeleteGlass}
+        >
+          No, Keep It
         </button>
       </div>
     </div>
