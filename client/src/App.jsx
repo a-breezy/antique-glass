27ae0cdf19cfdb6c6214dcf40c74c79bfa477cad
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2023-12-06 12:25:56 -0500

    change Home to Dashboard to create new homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import CreateGlass from './pages/CreateGlass'
 import DeleteGlass from './pages/DeleteGlass'
 import EditGlass from './pages/EditGlass'
 import ShowGlass from './pages/ShowGlass'
-import Home from './pages/Home'
+import Home from './pages/Dashboard'
+import Dashboard from './pages/Dashboard'
 
 export const App = () => {
   return (
     <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/glass/create' element={<CreateGlass />} />
         <Route path='/glass/details/:id' element={<ShowGlass />} />
         <Route path='/glass/edit/:id' element={<EditGlass />} />
