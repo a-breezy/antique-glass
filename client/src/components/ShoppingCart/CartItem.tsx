be1e4d67903416243437855b7f98f3e0ada2fbd2
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-02-29 16:45:40 -0500

    changed the cart structure so that its a bit cleaner and formats properly

--- client/src/components/ShoppingCart/CartItem.tsx
+++ client/src/components/ShoppingCart/CartItem.tsx
@@ -19,30 +19,32 @@ export function CartItem({ id, quantity }: CartItemProps) {
 
   const cartItem: CartItem | undefined = storeItems.find((i) => i._id === id);
   if (cartItem == null) return null;
-  
-  console.log(cartItem)
+
   return (
     <div className="flex flex-row h-1/4 max-h-40 my-4">
       <div className="w-1/2 h-40">
         <img src={cartItem.image} className="h-full m-auto" />
       </div>
+
       <div className="px-2 pb-2 w-1/2 flex flex-col justify-between">
-        <div className="flex justify-between">
-          <div>
+        <div className="flex justify-between flex-row w-full flex-wrap">
+          <div className="flex basis-full justify-between">
             <h3>{cartItem.title} Glass</h3>
-            {/* get the price of item and multiply by the quantity in cart */}
-            <p>${(cartItem.price / cartItem.quantity) * quantity}</p>
-            <div>
-              <p>Quantity: {quantity}</p>
-            </div>
-          </div>
-          <div>
             <button
               className="px-1 border border-black rounded font-semibold hover:bg-red-300"
               onClick={() => removeFromCart(cartItem._id)}
             >
               X
             </button>
+          </div>
+          <div className="flex basis-full justify-between">
+            {/* get the price of item and multiply by the quantity in cart */}
+            <p>Price</p>
+            <p>${(cartItem.price / cartItem.quantity) * quantity}</p>
+          </div>
+          <div className="flex basis-full justify-between">
+            <p>Quantity</p>
+            <p>{quantity}</p>
           </div>
         </div>
         <QuantityButton id={id} quantity={cartItem.quantity} style={"cart"} />
--- client/src/components/ShoppingCart/ShoppingCart.tsx
+++ client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -7,14 +7,15 @@ export default function ShoppingCart() {
   return (
     <div className="w-1/3 absolute z-10 p-2 right-0 top-24 bg-gray-200 border-l-2 border-b-2 border-solid border-black rounded-bl-lg">
       <div className="flex justify-between pb-2">
-        <h2 className="text-xl basis-2/3">Shopping cart</h2>
-        <button className="mr-2" onClick={closeCart}>
+        <h2 className="text-xl  w-1/2 text-center">Shopping cart</h2>
+        <button className="mr-2 hover:text-red-500" onClick={closeCart}>
           Close Cart
         </button>
       </div>
-        {cartItems.map((item) => (
-            <CartItem key={item.id} {...item} />
-        ))}
+      {cartItems.map((item) => {
+        console.log(cartItems.length);
+        return <CartItem key={item.id} {...item} />;
+      })}
     </div>
   );
 }
