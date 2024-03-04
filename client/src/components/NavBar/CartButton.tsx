0def075519522aca49758d1619f41f0803124f1c
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-03-04 17:19:46 -0500

    reformated store and about pages. Also moved CartButton to its own component

---
+++ client/src/components/NavBar/CartButton.tsx
@@ -0,0 +1,22 @@
+import { IoCartOutline, IoCart } from "react-icons/io5";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
+
+export default function CartButton() {
+  const { cartQuantity, toggleCart } = useShoppingCart();
+
+  return (
+        <button className="w-full h-full relative" onClick={toggleCart}>
+          {!cartQuantity ? (
+            <IoCartOutline className="w-full h-full" />
+          ) : (
+            <div>
+              <IoCart className="w-full h-full" />
+              <div className="absolute bottom-1.5 -right-1 w-6 rounded-full bg-red-400 text-white">
+                {cartQuantity}
+              </div>
+            </div>
+          )}
+        </button>
+
+  );
+}
--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -2,41 +2,30 @@ import { Link } from "react-router-dom";
 import { IoCartOutline, IoCart } from "react-icons/io5";
 import { useShoppingCart } from "../../context/ShoppingCartContext";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
+import CartButton from "./CartButton";
 
 export default function NavBar() {
   const { cartQuantity, isOpen, toggleCart } = useShoppingCart();
 
   return (
     <div className="flex justify-between flex-wrap shadow-lg p-4 bg-slate-200">
-      <div className="flex flex-col text-center">
-        <Link to="/">
-          <h1 className="text-3xl">Antique Glass</h1>
-        </Link>
-        <h2 className="text-xl">Your Place for Unique Glassware</h2>
-      </div>
-
-      {/* add buttons for various pages */}
-      <div className="flex flex-row justify-items-start items-center">
-        <h2 className="px-2">Store</h2>
-        <h2 className="px-2">About</h2>
+      <div className="flex flex-row w-1/2 justify-around">
+        <div className="flex flex-col text-center">
+          <Link to="/">
+            <h1 className="text-3xl">Antique Glass</h1>
+          </Link>
+          <h2 className="text-xl">Your Place for Unique Glassware</h2>
+        </div>
+        {/* add buttons for various pages */}
+        <div className="flex flex-row w-1/3 justify-around items-center">
+          <h2 className="px-2">Store</h2>
+          <h2 className="px-2">About</h2>
+        </div>
       </div>
 
       <div className="flex flex-wrap content-center w-11">
-        <button className="w-full h-full relative" onClick={toggleCart}>
-          {!cartQuantity ? (
-            <IoCartOutline className="w-full h-full" />
-          ) : (
-            <div>
-              <IoCart className="w-full h-full" />
-              <div className="absolute bottom-1.5 -right-1 w-6 rounded-full bg-red-400 text-white">
-                {cartQuantity}
-              </div>
-            </div>
-          )}
-        </button>
-        {isOpen && cartQuantity ? (
-          <ShoppingCart />
-        ) : null}
+        <CartButton />
+        {isOpen && cartQuantity ? <ShoppingCart /> : null}
       </div>
     </div>
   );
