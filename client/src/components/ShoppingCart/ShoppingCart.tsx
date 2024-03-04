47b8eaddef8e204d5765267232c4fedd21d97db9
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-03-04 17:09:46 -0500

    Added total price to shopping cart

--- client/src/components/ShoppingCart/ShoppingCart.tsx
+++ client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -2,7 +2,13 @@ import { useShoppingCart } from "../../context/ShoppingCartContext";
 import { CartItem } from "./CartItem";
 
 export default function ShoppingCart() {
-  const { closeCart, cartItems } = useShoppingCart();
+  const { closeCart, cartItems, storeItems } = useShoppingCart();
+
+  const totalPrice: number = cartItems.reduce((total, cartItem) => {
+    const item = storeItems.find((i) => i._id === cartItem.id);
+    let singleItem = item ? item.price / item.quantity : undefined;
+    return total + (singleItem ? singleItem * cartItem.quantity : 0);
+  }, 0);
 
   return (
     <div className="w-1/3 absolute z-10 p-2 right-0 top-24 bg-gray-200 border-l-2 border-b-2 border-solid border-black rounded-bl-lg">
@@ -12,10 +18,13 @@ export default function ShoppingCart() {
           Close Cart
         </button>
       </div>
-      {cartItems.map((item) => {
-        console.log(cartItems.length);
-        return <CartItem key={item.id} {...item} />;
-      })}
+      {cartItems.map((item) => (
+        <CartItem key={item.id} {...item} />
+      ))}
+      <div className="flex justify-between pb-2">
+        <h3 className="text-lg w-1/2 text-center">Total:</h3>
+        <h3 className="text-md">${totalPrice}</h3>
+      </div>
     </div>
   );
 }
