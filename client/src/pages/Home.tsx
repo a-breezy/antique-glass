3a16ab27a03df3d19ff2ee8b5c60657a8022a9d8
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-03-07 17:56:53 -0500

    added banner and styled button with link to store

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useState, Fragment } from "react";
 import { SingleGlass } from "../components/Home/SingleGlass";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   // const [glasses, setGlasses] = useState<GlassItem[] | null>([]);
@@ -14,12 +15,14 @@ export default function Home() {
           backgroundImage: "url(/assets/imgs/home-banner.jpg)",
           width: "100vw",
           height: "calc(100vh - 9rem)",
-          backgroundSize: "100%",
-          backgroundPosition: "center bottom"
         }}
+        className="flex flex-wrap place-content-center bg-no-repeat lg:bg-center-bottom bg-center bg-cover"
       >
-        test
-        {/* <img src={banner} /> */}
+        <div className="flex flex-wrap place-content-center border border-slate-600 bg-slate-200 hover:bg-slate-400 opacity-80 hover:opacity-70 w-60 h-24">
+          <Link to="/store">
+            <h2 className="font-mono">Come see our store</h2>
+          </Link>
+        </div>
       </div>
     </div>
   );
