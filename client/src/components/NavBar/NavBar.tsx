703d603098ae4a4a9e9aeed56bb20f11a4b93926
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-03-11 14:17:38 -0400

    updated mobile style

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -7,25 +7,22 @@ export default function NavBar() {
   const { cartQuantity, isOpen } = useShoppingCart();
 
   return (
-    <div className="flex justify-between flex-wrap lg:h-36 shadow-lg p-4 bg-slate-200">
-      <div className="flex flex-row w-1/2 justify-around">
-        <div className="flex flex-col flex-wrap place-content-center text-center">
-          <Link to="/">
-            <h1 className="text-3xl font-serif">Antique Glass</h1>
-            <h2 className="text-xl">Your Place for Unique Glassware</h2>
-          </Link>
-        </div>
-        {/* add buttons for various pages */}
-        <div className="flex lg:flex-row flex-col w-1/3 lg:justify-around justify-center items-center">
-          <Link to="/store">
-            <h2 className="px-2">Store</h2>
-          </Link>
-          <Link to="/about">
-            <h2 className="px-2">About</h2>
-          </Link>
-        </div>
+    <div className="flex justify-between flex-row flex-wrap lg:h-36 h-48 shadow-lg p-4 bg-slate-200">
+      <div className="flex flex-col flex-wrap place-content-center text-center">
+        <Link to="/">
+          <h1 className="text-3xl font-serif">Antique Glass</h1>
+          <h2 className="text-xl">Your Place for Unique Glassware</h2>
+        </Link>
       </div>
-
+      {/* add buttons for various pages */}
+      <div className="flex flex-row w-1/3 lg:justify-around justify-center items-center">
+        <Link to="/store">
+          <h2 className="px-2">Store</h2>
+        </Link>
+        <Link to="/about">
+          <h2 className="px-2">About</h2>
+        </Link>
+      </div>
       <div className="flex flex-wrap content-center w-11">
         <CartButton />
         {isOpen && cartQuantity ? <ShoppingCart /> : null}
--- client/src/components/ShoppingCart/ShoppingCart.tsx
+++ client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -11,7 +11,7 @@ export default function ShoppingCart() {
   }, 0);
 
   return (
-    <div className="w-1/3 absolute z-10 p-2 right-0 top-24 bg-gray-200 border-l-2 border-b-2 border-solid border-black rounded-bl-lg">
+    <div className="lg:w-1/3 absolute z-10 p-2 right-0 lg:top-36 top-48 bg-gray-200 lg:border-l-2 lg:border-b-2 border-solid border-black rounded-bl-lg rounded-br-lg">
       <div className="flex justify-between pb-2">
         <h2 className="text-xl  w-1/2 text-center">Shopping cart</h2>
         <button className="mr-2 hover:text-red-500" onClick={closeCart}>
